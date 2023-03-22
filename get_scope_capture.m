f579af4dfbceb6703f19beb5c1bac5db2ac0ac8f
Arthur Vie <el20a2v@leeds.ac.uk>
2023-03-22 12:45:19 +0000

    adapted for FSK measurements

--- get_scope_capture.m
+++ get_scope_capture.m
@@ -3,6 +3,17 @@ clear;
 tcpip = tcpclient('192.168.0.20', 5555);
 query(tcpip, "*IDN?")
 
+writeline(tcpip, ':RUN');
+writeline(tcpip, ':SINGle');
+
+trigger_status = query(tcpip, ':TRIG:STAT?')
+while ~strcmp(trigger_status, "STOP" + newline)
+    trigger_status = query(tcpip, ':TRIG:STAT?')
+end
+
+writeline(tcpip, ':WAV:MODE RAW');
+writeline(tcpip, ':WAV:STOP 500000');
+
 string = query(tcpip, ":WAV:DATA?");
 ascii_data = string(12:end);
 
@@ -15,9 +26,10 @@ for i = 1:length(ascii_data)
     data(i) = uint8(ascii_data(i));
 end
 
-data = data - 127;
-data = data * y_increment * 1000;
+data = data - 128;
+data = data * y_increment;
 data = data(1:end - 1);
+
 timebase_vector = 0:x_increment:(x_increment * (length(data) - 1));
 
 plot(timebase_vector, data);
@@ -25,4 +37,10 @@ title("Oscilloscope Waveform Captured Over LXI");
 ylabel("Voltage (mV)");
 xlabel("time (s)");
 
+difference = zeros(length(data), 1);
+for i = 1:(length(data) - 1)
+    difference(i) = data(i + 1) - data(i);
+end
 
+hold
+plot(timebase_vector, difference);
